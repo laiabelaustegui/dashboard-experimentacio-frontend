b334d9f4edb35b6773393a8c9d71da8c80cdfc01
laiabelaustegui <laia.nuria.belaustegui@estudiantat.upc.edu>
2026-01-07 22:54:35 +0100

    changed api url

--- src/providers/api.ts
+++ src/providers/api.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosInstance, type AxiosRequestConfig, AxiosError } from 'axios';
 import { toaster } from '@/components/ui/toaster';
 
-const BASE_URL = 'http://127.0.0.1:8000/';
+const BASE_URL = 'http://127.0.0.1:8000/api/';
 
 export class ApiError extends Error {
     constructor(
